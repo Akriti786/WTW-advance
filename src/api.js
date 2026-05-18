e056d7736a4e849e8d983b201f9c6e5bdbe3f3de
Akriti Shukla <115546336+Akriti786@users.noreply.github.com>
2026-05-18 14:04:38 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,21 +1,10 @@
-// frontend/api.js
-// import axios from "axios";
-
-// // For Android Emulator
-// export const API = "https://movies-backend-2twf.onrender.com";
-
-// export const api = axios.create({
-//   baseURL: API,
-//   timeout: 60000,
-// });
-
 import axios from "axios";
 
 export const API = "https://movies-backend-2twf.onrender.com";
 
 export const api = axios.create({
   baseURL: API,
-  timeout: 60000, // ✅ 60 seconds (Render cold start safe)
+  timeout: 60000,
 });
 
 // Optional: log errors
